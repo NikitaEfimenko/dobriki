726b5369a4a3f261c5d7feb5ac5b908d89b51ffc
Regelity <mszx2000@gmail.com>
2023-09-26 21:05:49 +0300

    fix

--- web/src/app/providers.tsx
+++ web/src/app/providers.tsx
@@ -1,16 +1,16 @@
 'use client'
 import { useState, ReactNode, useEffect } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { register, unregister } from "../config/serviceWorkerRegistration"
+// import { register, unregister } from "../config/serviceWorkerRegistration"
 
 export default function Providers({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient())
 
-  useEffect(() => {
-    register()
-    console.log("register!")
-    return () => unregister()
-  }, []);
+  // useEffect(() => {
+  //   register()
+  //   console.log("register!")
+  //   return () => unregister()
+  // }, []);
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
