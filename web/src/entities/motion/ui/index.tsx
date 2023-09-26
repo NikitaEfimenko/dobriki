d9df566c422fdb2c4cf3b2d3351bff6851808c4a
Regelity <mszx2000@gmail.com>
2023-09-26 21:22:38 +0300

    fix

--- web/src/entities/motion/ui/index.tsx
+++ web/src/entities/motion/ui/index.tsx
@@ -4,20 +4,27 @@ import { PluginListenerHandle } from '@capacitor/core';
 import { Motion } from '@capacitor/motion';
 import { useEffect } from 'react';
 import type { AccelListenerEvent } from '@capacitor/motion';
+import { handleAccelerometerReading } from '@/shared/utils';
 
 
 const MotionComp = () => {
   const [motion, setMotion] = useState<AccelListenerEvent>()
+  const [stepCount, setStepCount] = useState<number>(0)
+  const [error, setError] = useState("")
 
   useEffect(() => {
+    
     (async () => {
       try {
         const accelHandler: PluginListenerHandle = await Motion.addListener("accel", event => {
           console.log('Device motion event:', event);
           setMotion(event)
+          const acc = event.acceleration
+          handleAccelerometerReading(acc.x, acc.y, acc.z, setStepCount)
         });
       } catch (e) {
         console.log(e)
+        setError(e as string)
       }
     })()
   }, [])
@@ -26,6 +33,8 @@ const MotionComp = () => {
     <ul>
       <li>
         motion: {JSON.stringify(motion)}
+        step-count {stepCount}
+        error - {error}
       </li>
       <li>
       </li>
