f7ce5ccca422e0afc78795023ae5293eced2e968
Regelity <mszx2000@gmail.com>
2023-09-29 18:05:25 +0300

    fix

--- web/src/widgets/challenges-carousel/ui/index.tsx
+++ web/src/widgets/challenges-carousel/ui/index.tsx
@@ -5,7 +5,7 @@ import { carouselConfig } from "../config"
 import { useMemo } from "react"
 
 export const ChallengesCarousel = () => {
-  const list = useMemo(() => carouselConfig.map((config) => <ChallengeCard {...config}/>), [])
+  const list = useMemo(() => carouselConfig.map((config, idx) => <ChallengeCard key={idx} {...config}/>), [])
   return <>
     <Carousel list={list} />
   </>
