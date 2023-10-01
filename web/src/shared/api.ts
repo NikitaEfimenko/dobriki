bd5177baa5ccb154de11f1f4c71f3e764a24c300
Regelity <mszx2000@gmail.com>
2023-10-01 03:31:35 +0300

    fix

--- web/src/shared/api.ts
+++ web/src/shared/api.ts
@@ -1,3 +1,4 @@
-import { FeedApi } from "../shared/api/api";
+import { FeedApi, CharityApi } from "../shared/api/api";
 
 export const feedApiInstance = new FeedApi();
+export const charityApiInstance = new CharityApi();
